cb9764a192e68264f4be6ed1d846d0904d6ab749
theo shin <t8shikuro@gmail.com>
2020-04-27 20:54:46 -0400

    project folder with completed jupyter notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classifying whether a patient has heart disease based on patient data',
+    author='Ted Shikuro',
+    license='',
+)
